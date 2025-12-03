fix normalization.  fix NaN phases.

src/mini_pvdr.py
@@ -6,7 +6,10 @@ import wave
 import numpy as np
 
 
-def calcPhase(pa0, cs0, cs1, ra, rs):
+def phase_diff(x, y):
+    return cmath.phase(x / y) if y != 0.0 else 0.0
+
+def calc_pghi(pa0, cs0, cs1, ra, rs):
     assert len(pa0) == len(cs0) and len(cs0) == len(cs1)
 
     ratio = rs / ra
@@ -20,20 +23,20 @@ def calcPhase(pa0, cs0, cs1, ra, rs):
         _, i, isPrev = heapq.heappop(heap)
         if isPrev:
             if empty[i]:
-                dp = cmath.phase((cs1[i] / cs0[i]) * cmath.exp((-2.0j * cmath.pi * ra) * i))
+                dp = phase_diff(cmath.exp((-2.0j * cmath.pi * ra) * i) * cs1[i], cs0[i])
                 pa1[i] = pa0[i] + ratio * dp + (2.0 * cmath.pi * rs) * i
                 empty[i] = False
                 heapq.heappush(heap, (-abs(cs1[i]), i, False))
                 count += 1
         else:
             if i >= 1 and empty[i - 1]:
-                dp = cmath.phase(cs1[i - 1] / cs1[i])
+                dp = phase_diff(cs1[i - 1], cs1[i])
                 pa1[i - 1] = pa1[i] + ratio * dp
                 empty[i - 1] = False
                 heapq.heappush(heap, (-abs(cs1[i - 1]), i - 1, False))
                 count += 1
             if i < len(cs1) - 1 and empty[i + 1]:
-                dp = cmath.phase(cs1[i + 1] / cs1[i])
+                dp = phase_diff(cs1[i + 1], cs1[i])
                 pa1[i + 1] = pa1[i] + ratio * dp
                 empty[i + 1] = False
                 heapq.heappush(heap, (-abs(cs1[i + 1]), i + 1, False))
@@ -43,10 +46,10 @@ def calcPhase(pa0, cs0, cs1, ra, rs):
     return pa1
 
 def process(fa, Aa, La, As, Ls, Mf):
-    assert La % 2 == 0 and Ls % 2 == 0
+    assert La % 2 == 0 and Ls % 2 == 0 and Ls % (4 * As) == 0
     N = (len(fa) - La) // Aa
 
-    window = np.sqrt(2.0 / 3.0) * np.sin((np.pi / La) * np.arange(La)) ** 2
+    window = np.sqrt(8.0 / 3.0) * np.sin((np.pi / La) * np.arange(La)) ** 2
     ca = np.empty([La * Mf // 2 + 1, N], np.complex128)
     for i in range(N):
         windowed = window * fa[Aa * i : Aa * i + La]
@@ -56,16 +59,16 @@ def process(fa, Aa, La, As, Ls, Mf):
     cs[:, 0] = ca[:, 0]
     ps = np.angle(ca[:, 0])
     for i in range(1, N):
-        ps = calcPhase(ps, ca[:, i - 1], ca[:, i], Aa / (La * Mf), As / (Ls * Mf))
+        ps = calc_pghi(ps, ca[:, i - 1], ca[:, i], Aa / (La * Mf), As / (Ls * Mf))
         cs[:, i] = np.abs(ca[:, i]) * np.exp(1.0j * ps)
 
-    window = np.sqrt(2.0 / 3.0) * np.sin((np.pi / Ls) * np.arange(Ls)) ** 2
+    window = np.sqrt(8.0 / 3.0) * np.sin((np.pi / Ls) * np.arange(Ls)) ** 2
     fs = np.zeros(As * N + Ls)
     for i in range(N):
         inversed = np.fft.irfft(np.r_[cs[: Ls * Mf // 2 + 1, i], np.zeros(max((Ls - La) * Mf // 2, 0))], norm = "forward")
         fs[As * i : As * i + Ls] += window * np.r_[inversed[-Ls // 2 :], inversed[: Ls // 2]]
 
-    return fs
+    return (As / Ls) * fs
 
 def main():
     if len(sys.argv) != 5:
@@ -80,6 +83,7 @@ def main():
 
     kt, kp = float(sys.argv[1]), float(sys.argv[2])
     fs = process(fa, round(1024.0 / (kt * kp)), 4096, round(1024.0 / kp), 4 * round(1024.0 / kp), 2)
+    #fs = process(fa, round(1024.0 / kt), 2 * round(2048.0 * kp), 1024, 4096, 2)
 
     fs = np.clip(np.round(32768.0 * fs), -32768.0, 32767.0).astype("<h")
     with wave.open(sys.argv[4], "wb") as f: